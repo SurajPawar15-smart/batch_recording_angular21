fba171b689a56a67fe36a54cc3d8e474f41f5798
SurajPawar15-smart <supersuraj143@gmail.com>
2025-12-30 09:04:55 +0530

    added the bootstrap and components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Layout } from './pages/layout/layout';
+import { BatchMaster } from './pages/batch-master/batch-master';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'admin', component: Login },
+  {
+    path: '',
+    component: Layout,
+    children: [
+      { path: 'batch', component: BatchMaster },
+      { path: 'dashboard', component: BatchMaster },
+    ],
+  },
+];
